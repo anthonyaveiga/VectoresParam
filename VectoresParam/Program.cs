1b1303341c0f8e1f6f914caf5e52c0c0e72ab654
Anthony <anthony.aveiga@pucese.edu.ec>
2018-12-06 09:21:25 -0500

    commit

---
+++ VectoresParam/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectoresParam
+{
+    class Program
+    {
+        const int MAX = 10;
+        static void Main(string[] args)
+        {
+            int[] x = new int[MAX];
+            leer(x);
+            imprimir(x);
+            Console.ReadKey();
+        }
+
+        public static void leer(int[] x)
+        {
+            for (int i = 0; i < MAX; i++)
+            {
+                Console.WriteLine("Ingrese el elemento {0}", i + 1);
+                x[i] = Int32.Parse(Console.ReadLine());
+            }
+        }
+        public static void imprimir(int[] x)
+        {
+            foreach (int num in x)
+            {
+                Console.WriteLine("Elemento {0}", num);
+            }
+        }
+    }
+}
